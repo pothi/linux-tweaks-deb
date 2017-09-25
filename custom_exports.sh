87036291c50ac8d9b6a11df9355055d633f285c6
Pothi Kalimuthu <pothi@tinywp.com>
2017-09-25 10:49:30 +0530

    Add path to local npm bin

--- custom_exports.sh
+++ custom_exports.sh
@@ -10,6 +10,6 @@ export HISTCONTROL=$HISTCONTROL:erasedups:ignorespace
 export EDITOR=vi
 export VISUAL=vi
 
-export PATH=/usr/local/bin:~/.composer/vendor/bin:$PATH
+export PATH=~/.npm-global/bin:~/.composer/vendor/bin:/usr/local/bin:$PATH
 
 ### end of custom exports by pothi ###
