bd160c98c6c39280830f8e7645666820e93857d6
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-08-19 14:06:27 +0530

    Accept all encoding for curl

--- common-aliases.sh
+++ common-aliases.sh
@@ -17,8 +17,8 @@ alias lla='lal'
 alias llh='ll'
 
 # curl
-alias curld='curl -H "Accept-Encoding:gzip,deflate" -s -D- -o /dev/null -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0"'
-alias curlm='curl -H "Accept-Encoding:gzip,deflate" -s -D- -o /dev/null -A "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"'
+alias curld='curl -H "Accept-Encoding:*" -s -D- -o /dev/null -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0"'
+alias curlm='curl -H "Accept-Encoding:*" -s -D- -o /dev/null -A "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4"'
 alias curll='curl -L'
 
 # Dig aliases
