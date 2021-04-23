bcf5b8a9957550fcdfbb9f668d5e043125824487
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2021-04-23 18:25:04 +0530

    Update EXPORTs and PATHs

--- common-exports.sh
+++ common-exports.sh
@@ -11,34 +11,40 @@ export LANG="en_US.UTF-8"
 export LC_ALL=$LANG
 
 export GEM_HOME=~/.gem
+export GEM_PATH=$GEM_HOME/bin
 
 # for history control - bash specific
 export HISTCONTROL=$HISTCONTROL:erasedups:ignorespace
 
+# Go WorkSpace
+export GOPATH=$HOME/.local/go-ws
+
 #-------------------------- PATH ------------------------#
 
-# dnsmasq
+[ -d $GEM_PATH ] && PATH=$GEM_PATH:$PATH
+
 # export LOC_BIN_PATH=/usr/local/bin
-export LOC_SBIN_PATH=/usr/local/sbin
+# export LOC_SBIN_PATH=/usr/local/sbin
+
+# set PATH so it includes user's GO lang workspace bin if it exists
+[ -d "$(go env GOPATH)/bin" ] && PATH="$(go env GOPATH)/bin:$PATH"
 
 # PHP composer
-export COMPOSER_PATH=~/.composer/vendor/bin
+# export COMPOSER_PATH=~/.composer/vendor/bin
+[ -d "$HOME/.composer/vendor/bin" ] && PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # user bin and scripts
-export USER_PATH=~/bin:~/scripts
-
-# GEM
-export GEM_PATH=~/.gem/bin
+# export USER_PATH=~/bin:~/scripts
 
 # python libraries installed as python install xyz --user
-export PYTHON_USER_PATH=~/Library/Python/2.7/bin
+# export PYTHON_USER_PATH=~/Library/Python/2.7/bin
 # mac ports
-export MAC_PORTS_PATH=/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin
+# export MAC_PORTS_PATH=/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin
 
 # npm
-export CUSTOM_NPM_PATH=~/.npm-global/bin
+# export CUSTOM_NPM_PATH=~/.npm-global/bin
 # yarn
 # export PREFIX=~/.config/yarn
 
-export PATH=~/.local/bin:$CUSTOM_NPM_PATH:$PYTHON_USER_PATH:$GEM_PATH:$USER_PATH:$COMPOSER_PATH:$LOC_SBIN_PATH:$PATH
-[[ $IS_MAC = 1 ]] && export PATH=$MAC_PORTS_PATH:$PATH
+# export PATH=~/.local/bin:$CUSTOM_NPM_PATH:$PYTHON_USER_PATH:$GEM_PATH:$USER_PATH:$COMPOSER_PATH:$LOC_SBIN_PATH:$PATH
+# [[ $IS_MAC = 1 ]] && export PATH=$MAC_PORTS_PATH:$PATH
